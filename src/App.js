6b82082baa30517566b5c5dc8fd9b6db1dcbb80a
x2Dzi <bojan.dukovski211@gmail.com>
2022-08-24 03:48:15 +0200

    under construstion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div style={styles} className={'mt-5 text-center container'}>
-      <p>The website is under construction!</p>
-    </div>
-    </div>
-  );
+    const styles = {
+        backgroundColor: 'pink',
+        color: 'darkred',
+        border: 'solid darkred 15px',
+        borderRadius: '20px',
+        fontSize: '90px',
+        fontWeight: 'bold',
+    };
+    return (
+        <div style={styles} className={'mt-5 text-center container'}>
+            <p>The website is under construction!</p>
+        </div>
+    );
 }
 
 export default App;
