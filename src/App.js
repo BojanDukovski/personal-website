38328a46d1e42872f421e11f9027a6b65abf084d
x2Dzi <bojan.dukovski211@gmail.com>
2022-08-24 03:43:47 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={styles} className={'mt-5 text-center container'}>
+      <p>The website is under construction!</p>
+    </div>
     </div>
   );
 }
